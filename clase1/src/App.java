bf095a7ae1d2e8d9f856a0ed56ec46c2d41e7003
jeidymurillo <jeidy.murillo@correounivalle.edu.co>
2024-02-29 10:59:56 -0500

    Agregar la clase 1

---
+++ clase1/src/App.java
@@ -0,0 +1,21 @@
+import javax.swing.JOptionPane;
+
+public class App {
+    //psvm - atajo
+    public static void main(String[] args) throws Exception {
+        //atajo sout - string de para imprimir
+        /*System.out.println("Hello, World!");*/
+        //JOptionPane.showMessageDialog(null, "BTS-ARMY", "KPOP", JOptionPane.WARNING_MESSAGE);
+
+        // Se puede almecenar en un rango de n numeros negativos y positivo
+
+        byte edad = 127;
+        System.out.println(edad);
+
+        boolean ok = false;
+
+        char c = 'f';
+        //Se pueden utilizar metodos, String no es primivita - un arreglo de char
+        String s = "Selenar";
+        }
+}
